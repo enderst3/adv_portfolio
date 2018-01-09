caca1a1cb80651ef0a0023d5b02285c8b4a96cc8
enderst3 <enderst3@gmail.com>
2018-01-09 10:09:22 -0800

    blueprint, added mongo

--- docker_blueprint/web/app.py
+++ docker_blueprint/web/app.py
@@ -13,6 +13,3 @@ app.register_blueprint(user, url_prefix='/user/logout')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
-
-
--- docker_blueprint/web/user/controllers.py
+++ docker_blueprint/web/user/controllers.py
@@ -1,15 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
 
 
 user = Blueprint('user', __name__, template_folder='templates')
 
 
-@user.route('/')
-def user_home():
-    return render_template('index.html')
+@user.route('/', defaults={'page': 'index'})
+@user.route('/logout', defaults={'page': 'logout'})
+# def logout():
+#     return render_template('logout.html')
+
+# # =========================================
+#
+#
+# # @simple_page.route('/', defaults={'page': 'index'})
+# # @simple_page.route('/<page>')
+# # def show(page):
+# #     try:
+# #         return render_template('pages/%s.html' % page)
+# #     except TemplateNotFound:
+# #         abort(404)
+
+# @user.route('/', defaults={'page': 'index'})
+@user.route('/<page>')
+def show(page):
+    try:
+        return render_template('%s.html' % page, page=page)
+    except TemplateNotFound:
+        abort(404)
 
 
-@user.route('/logout')
-def logout():
-    return render_template('logout.html')
-
+# @user.errorhandler(404)
+# def page_not_found(e):
+#     return 'Page Not Found'
