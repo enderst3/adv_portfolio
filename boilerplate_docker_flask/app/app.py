f6aa6437b6d9b7f9a830cf02f1a54bcf959ccf1b
enderst3 <enderst3@gmail.com>
2018-01-15 20:20:45 -0800

    error handling

--- boilerplate_docker_flask/app/app.py
+++ boilerplate_docker_flask/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from app.blueprints.page import page
 from app.blueprints.admin import admin
@@ -18,6 +18,7 @@ def create_app():
 
     app.logger.setLevel(app.config['LOG_LEVEL'])
 
+    error_templates(app)
     app.register_blueprint(page)
     app.register_blueprint(admin)
     extensions(app)
@@ -32,5 +33,32 @@ def extensions(app):
     :return: None
     """
     debug_toolbar.init_app(app)
+
+    return None
+
+def error_templates(app):
+    """
+    Register 0 or more custom error pages (mutates the app passed in).
+
+    :param app: Flask application instance
+    :return: None
+    """
+
+    def render_status(status):
+        """
+         Render a custom template for a specific status.
+           Source: http://stackoverflow.com/a/30108946
+
+         :param status: Status as a written name
+         :type status: str
+         :return: None
+         """
+        # Get the status code from the status, default to a 500 so that we
+        # catch all types of errors and treat them as a 500.
+        code = getattr(status, 'code', 500)
+        return render_template('errors/{0}.html'.format(code)), code
+
+    for error in [404, 500]:
+        app.errorhandler(error)(render_status)
 
     return None
