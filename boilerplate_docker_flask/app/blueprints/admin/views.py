2756f288818c6c0903bbd9bd63e69a9b2706a512
enderst3 <enderst3@gmail.com>
2018-01-19 16:01:55 -0800

    added bootstap

--- boilerplate_docker_flask/app/blueprints/admin/views.py
+++ boilerplate_docker_flask/app/blueprints/admin/views.py
@@ -3,9 +3,9 @@ from flask import Blueprint, render_template
 admin = Blueprint('admin', __name__, template_folder='templates')
 
 @admin.route('/admin')
-def home():
-    return render_template('admin.html')
+def index():
+    return render_template('admin/admin.html')
 
 @admin.route('/users')
-def terms():
-    return render_template('users.html') 
+def users():
+    return render_template('admin/users.html') 
