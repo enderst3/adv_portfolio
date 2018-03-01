a3cfed35076286c7930a2a250c0a5d7278e727c2
enderst3 <enderst3@gmail.com>
2018-03-01 10:50:06 -0800

    working on task list tutorial

---
+++ task-app-test/src/CreateTask.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Well, FormControl, FormGroup, Button } from 'react-bootstrap'
+
+export default class CreatTask extends Component {
+  constructor (props) {
+    super(props)
+    this.handleSubmit = this.props.submitNewTask.bind(this)
+    this.handleChange = this.props.addTaskInput.bind(this)
+  }
+
+  render () {
+    return(
+      <Well className='add-item'>
+        <form 
+          onSubmit={this.handleSubmit}
+        >
+          <FormGroup>
+            <FormControl
+              type='text'
+              name='task'
+              placeholder='Create a Task...'
+              onChange={this.handleChange}
+              value={this.props.task}
+            />
+            <br />
+            <Button
+              type='submit'
+            >
+              Add To Do to List
+            </Button>
+          </FormGroup>
+        </form>
+      </Well>
+    )
+  }
+}
