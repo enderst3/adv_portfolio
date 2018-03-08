d3242c0362c3e2953c2fba65bbb7f6dd2add6a4e
enderst3 <enderst3@gmail.com>
2018-03-08 12:12:09 -0800

    added layout to app

--- task-list/src/App.js
+++ task-list/src/App.js
@@ -4,14 +4,26 @@ import './App.css'
 import firebase from './firebase'
 
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Col md={8} mdOffset={2}>
+          <Panel>
+          <Panel.Body>
+            <Jumbotron className='Header'>
+              <h1>Task List App</h1>
+            </Jumbotron>
+
+          </Panel.Body>
+          <Panel.Footer>
+            &copy;2018 Task List App
+          </Panel.Footer>
+       
+          </Panel>
+        </Col>
       </div>
     );
   }
 }
 
-export default App;
