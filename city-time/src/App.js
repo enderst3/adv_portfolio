b6942cad9a9ef18e3e5202d1d4f43fe6de582317
enderst3 <enderst3@gmail.com>
2018-03-22 15:15:02 -0700

    added city selecting, and UTC time

--- city-time/src/App.js
+++ city-time/src/App.js
@@ -3,11 +3,56 @@ import './App.css';
 
 const data = [
   {name: 'Portland', offset: 7},
-  {name: 'Miami', offset: 4}
+  {name: 'Miami', offset: 4},
+  {name: 'Denver', offset: 6}
 ]
 
-class App extends Component {
+
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      selectedCity: null,
+      selectedOffset: null,
+      currentUTC: null
+    }
+    this.chosenCity = this.chosenCity.bind(this)
+  }
+
+  chosenCity (item) {
+    console.log(item.name)
+    let now = new Date()
+    this.setState({
+      selectedCity: item.name,
+      selectedOffset: item.offset,
+      currentUTC: now.toUTCString()
+    })
+  }
+
   render() {
+    if (this.state.selectedCity || this.state.selectedOffset) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">City Time</h1>
+          </header>
+          <div>
+            <div>
+              <CityList
+                data={this.props.data}
+                chosenCity={this.chosenCity}
+              />
+            </div>
+            <div>
+              <p>The Current time in {this.state.selectedCity} is:</p>
+              <p>{this.state.currentUTC} - {this.state.selectedOffset} hours</p>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -17,15 +62,15 @@ class App extends Component {
           <div>
             <CityList
               data={this.props.data}
+              chosenCity={this.chosenCity}
             />
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
 
 const CityList = (props) => {
   const cityData = data.map((item) => {
@@ -33,6 +78,8 @@ const CityList = (props) => {
       <CityListItem
         key={item.name}
         item={item}
+        chosenCity={props.chosenCity}
+
       />
     )
   })
@@ -43,12 +90,26 @@ const CityList = (props) => {
   )
 }
 
-const CityListItem = (props) => {
-  return (
-    <div
-      key={props.item.city}
-    >
-      <p>{props.item.name}: {props.item.offset}</p>
-    </div>
-  )
+
+class CityListItem extends Component {
+  constructor (props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (e) {
+    this.props.chosenCity(this.props.item)
+  }
+
+  render () {
+    return (
+      <div
+        key={this.props.item.name}
+        onClick={this.handleClick}
+        value={this.props.item.name}
+      >
+        <p>{this.props.item.name}: {this.props.item.offset}</p>
+      </div>
+    )
+  }
 }
