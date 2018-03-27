f6e27841683a5a873930f9d9b6338ba16ef91db3
enderst3 <enderst3@gmail.com>
2018-03-27 11:15:18 -0700

    added bootstrap

--- flaskapp/web/app/__init__.py
+++ flaskapp/web/app/__init__.py
@@ -1,7 +1,10 @@
 import logging
 from flask import Flask
 from config import config
+from flask_bootstrap import Bootstrap
 
+
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -11,6 +14,7 @@ def create_app(config_name):
 
     init_logging(app)
 
+    bootstrap.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
