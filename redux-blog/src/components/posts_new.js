78baf5303a9cbfcb56e60511df35fa6cd6cd3df2
enderst3 <enderst3@gmail.com>
2018-06-20 14:29:21 -0700

    validation

--- redux-blog/src/components/posts_new.js
+++ redux-blog/src/components/posts_new.js
@@ -12,21 +12,32 @@ class PostsNew extends Component {
           type='text'
           {...field.input}
         />
+        {field.meta.error}
+        {field.meta.touched ? feild.meta.error : ''}
       </div>
     )
   }
 
+  onSubmit(values) {
+    console.log(values)
+  }
+
   render() {
+    const { handleSubmit } = this.props
     return (
-      <form>
+      <form 
+        onSubmit={
+          handleSubmit(this.onSubmit.bind(this))
+        }
+      >
         <Field
           label='Post Title'
           name='title'
           component={this.renderField}
         />
         <Field
-          label='Tags'
-          name='tags'
+          label='Categories'
+          name='categories'
           component={this.renderField}
         />
         <Field
@@ -34,11 +45,35 @@ class PostsNew extends Component {
           name='content'
           component={this.renderField}
         />
+        <button
+          type='submit'
+          className='btn btn-primary'
+          >Submit
+          </button>
       </form>
     )
   }
 }
 
+
+function validate(values) {
+  // console.log(values) -> { title: 'asdf', categories: ''asdfas, content: 'asdfasdf' }
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = "Enter a title."
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categries."
+  }
+  if (!values.content) {
+    errors.content = "Enter some blog content."
+  }
+  return errors
+}
+
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 }) (PostsNew)
