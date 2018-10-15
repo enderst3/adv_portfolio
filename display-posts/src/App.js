b62a4fc53c5894e6eebeebdef5f756c6521e3e5a
enderst3 <enderst3@gmail.com>
2018-10-14 19:11:17 -0700

    adding provider

--- display-posts/src/App.js
+++ display-posts/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux'
+
 
 import Posts from './components/Posts'
 import PostForm from './components/PostForm'
@@ -8,14 +10,16 @@ import PostForm from './components/PostForm'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Redux Posts</h1>
-        </header>
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider>
+        <div className="App">
+          <header className="App-header">
+            <h1>Redux Posts</h1>
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
