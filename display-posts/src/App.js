3b156a17979b3d9b2e760be5531509276b70ab9b
enderst3 <enderst3@gmail.com>
2018-10-15 21:48:30 -0700

    store

--- display-posts/src/App.js
+++ display-posts/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Provider } from 'react-redux'
-
+import { createStore, applyMiddleware  } from 'redux'
 
 import Posts from './components/Posts'
 import PostForm from './components/PostForm'
 
 
+
+const store = createStore(() => [], {}, applyMiddleware)
+
 class App extends Component {
   render() {
     return (
-      <Provider>
+      <Provider
+        store={store}
+      >
         <div className="App">
           <header className="App-header">
             <h1>Redux Posts</h1>
