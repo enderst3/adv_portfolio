bea51a2da5a54067e8fc7dd238ae1f76cf7a7d42
enderst3 <enderst3@gmail.com>
2018-10-19 19:13:00 -0700

    finished fetch posts added proptypes

--- display-posts/src/components/Posts.js
+++ display-posts/src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions/postActions'
 
@@ -10,7 +11,7 @@ class Posts extends Component {
   }
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -25,4 +26,12 @@ class Posts extends Component {
   }
 }
 
-export default connect(null, { fetchPosts })(Posts)
+Posts.propTypes = {
+  fetchPosts: PropTypes.func,
+  posts: PropTypes.array
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items
+})
+export default connect(mapStateToProps, { fetchPosts })(Posts)
